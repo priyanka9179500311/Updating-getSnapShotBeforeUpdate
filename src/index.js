bd903a729af62727a16ef11a7cb8ce52738d2817
priyanka yadav <py631588@gmail.com>
2021-01-07 10:41:18 +0530

    getSnapShotBeforeUpdate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+//import area
+
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create an Class Component
+class Myclass extends Component{
+    //1.Property
+    state = {};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //2.Construtor
+    //order sequence 1
+    constructor(props){
+        super(props);
+        //The Role Constructor is to initialize the properties
+        this.state = {"favColor":'red'};
+    }
+    getSnapshotBeforeUpdate(prevProps,prevState){//Formal argument
+        alert('Previous value of State was '+prevState.favColor);
+    }
+    componentDidUpdate(){
+        alert('After value of state is'+this.state.favColor);
+    }
+
+    //3.Method
+    //Every method must have render method
+    //order sequence 2
+    render(){
+        return(
+            <div>
+                <h1>My Favourite color is { this.state.favColor }</h1>
+            </div>
+        );
+    }
+    //order sequence 3
+    componentDidMount(){
+       //this.state.favColor = 'yellow';
+        setTimeout(()=>{
+            this.setState({"favColor":'yellow'});
+        },3000);
+    }
+
+}
+
+ReactDOM.render(<Myclass/>,document.getElementById('root'));//actual arg
